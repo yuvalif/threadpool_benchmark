43d9c34317b925445cb8dbb3b9e050992ac15b23
Yuval Lifshitz <yuvalif@yahoo.com>
2016-12-01 16:36:14 +0200

    added thread pool based on boost asio

--- benchmark_pool.cpp
+++ benchmark_pool.cpp
@@ -14,6 +14,7 @@
 #if BOOST_VERSION > 105600
 #include <boost/thread/executors/basic_thread_pool.hpp> // for testing new boost thread pool. available from ver 1.56 and up
 #endif
+#include "thread_pool.hpp" // for asio based pool
 
 // the work being done is calculating if a number is prime or no and accumulating the result
 bool IsPrime(unsigned long n)
@@ -54,6 +55,25 @@ void CountIfPrimeCTPL(int id, unsigned long n, std::atomic<unsigned long>& count
 void CountIfPrimeBoost(unsigned long n, std::atomic<unsigned long>& count)
 {
 	if (IsPrime(n)) ++count;
+}
+
+unsigned long count_primes_asio(const std::vector<unsigned long>& random_inputs, unsigned int NUMBER_OF_PROCS)
+{
+    std::atomic<unsigned long> number_of_primes(0);
+	{
+		thread_pool pool(NUMBER_OF_PROCS);
+
+		// loop over input to accumulate how many primes are there
+    	std::for_each(random_inputs.begin(), random_inputs.end(), 
+            [&](unsigned long n) 
+    	{
+    		while (!pool.run_task(boost::bind(CountIfPrimeBoost, n, std::ref(number_of_primes)))) {}
+    	});
+
+        // as pool go out of scope, it will wait for all threads to finish
+	}
+
+    return number_of_primes;
 }
 
 #if BOOST_VERSION > 105600
@@ -212,5 +232,11 @@ int main(int argc, char** argv)
     std::cout << "count_primes_boost:" << number_of_primes << " prime numbers were found. computation took " << 
         std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count()/INPUT_SIZE  << " nanosec per iteration" << std::endl;
 #endif
+
+    start = std::chrono::system_clock::now();
+    number_of_primes = count_primes_asio(random_inputs, NUMBER_OF_PROC);
+    end = std::chrono::system_clock::now();
+    std::cout << "count_primes_asio:" << number_of_primes << " prime numbers were found. computation took " << 
+        std::chrono::duration_cast<std::chrono::nanoseconds> (end - start).count()/INPUT_SIZE  << " nanosec per iteration" << std::endl;
     return 0;
 }
