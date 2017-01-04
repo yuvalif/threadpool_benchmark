02c4dfef41026cd089e8b67ee9304d3c60c46d61
Yuval Lifshitz <yuvalif@yahoo.com>
2017-01-04 11:08:43 +0100

    rearrange code better

--- mailbox_thread_pool.hpp
+++ mailbox_thread_pool.hpp
@@ -10,13 +10,56 @@ template<typename Arg, std::size_t QSIZE=512>
 class thread_pool
 {
 private:
-    std::size_t m_size;
-    bool m_done;
+    std::size_t m_number_of_workers;
+    volatile bool m_done;
     std::queue<Arg> m_queue;
     std::mutex m_mutex;
     std::condition_variable m_empty_cond;
     std::condition_variable m_full_cond;
     std::vector<std::thread> m_workers;
+
+    void worker(std::function<void(const Arg&)> F)
+    {
+        while (true)
+        {
+            Arg arg;
+            {
+                std::unique_lock<std::mutex> mlock(m_mutex);
+                // if queue is not empty we continue regardless of "done" indication
+                if (m_queue.empty())
+                {
+                    // queue is empty
+                    if (m_done)
+                    {
+                        // queue is empty and we are done
+                        return;
+                    }
+                    else
+                    {
+                        // queue is empty but we are not done yet
+                        // wait to get notified, either on queue not empty of being done
+                        while (!m_done && m_queue.empty())
+                        {
+                            m_empty_cond.wait(mlock);
+                        }
+                        if (m_queue.empty())
+                        {
+                            // done and empty
+                            return;
+                        }
+                    }
+               }
+
+               // there is work to do
+               arg = m_queue.front();
+               m_queue.pop();
+            }
+            // notify that queue is not full
+            m_full_cond.notify_one();
+            // execute the work when the mutex is not locked
+            F(arg);
+        }
+    }
 
 public:
 
@@ -33,51 +76,14 @@ public:
     }
 
     //! constructor spawn the threads
-    thread_pool(std::size_t size, const std::function<void(const Arg&)>& F) :
-        m_size(size),
+    thread_pool(std::size_t number_of_workers, std::function<void(const Arg&)> F) :
+        m_number_of_workers(number_of_workers),
         m_done(false)
     {
-        for (auto i = 0; i < m_size; ++i)
+        for (auto i = 0; i < m_number_of_workers; ++i)
         {
             // start all worker threads
-            m_workers.push_back(std::thread([this, F]()
-            {
-                while (true)
-                {
-                    Arg arg;
-                    {
-                        std::unique_lock<std::mutex> mlock(m_mutex);
-
-                        if (m_queue.empty())
-                        {
-                            // queue is empty
-                            if (m_done)
-                            {
-                                // queue is empty and we are done
-                                // notify any blocked submitter and exit
-                                mlock.unlock();
-                                m_full_cond.notify_one();
-                                return;
-                            }
-                            else
-                            {
-                                // queue is empty but we are done yet
-                                // wait to get notified, either on queue not empty of being done
-                                m_empty_cond.wait(mlock, []{return true;});
-                                // if there was a spurios wake-up, we need to check the conditions again
-                                continue;
-                            }
-                        }
-                        // there is work to do
-                        arg = m_queue.front();
-                        m_queue.pop();
-                    }
-                    // notify that queue is not full
-                    m_full_cond.notify_one();
-                    // execute the work when the mutex is not locked
-                    F(arg);
-                }
-            }));
+            m_workers.push_back(std::thread(&thread_pool::worker, this, F));
         }
     }
 
@@ -87,8 +93,8 @@ public:
     {
         {
             std::unique_lock<std::mutex> mlock(m_mutex);
+            while (!m_done && m_queue.size() == QSIZE) m_full_cond.wait(mlock);
             if (m_done) return;
-            while (m_queue.size() == QSIZE) m_full_cond.wait(mlock);
             m_queue.push(arg);
         }
         m_empty_cond.notify_one();
@@ -100,11 +106,10 @@ public:
     {
         {
             std::unique_lock<std::mutex> mlock(m_mutex);
-            if (m_done) return false;
-            if (m_queue.size() == QSIZE) return false;
+            if (m_done || m_queue.size() == QSIZE) return false;
             m_queue.push(arg);
         }
-       m_empty_cond.notify_one();
+        m_empty_cond.notify_one();
         return true;
     }
 
@@ -120,19 +125,19 @@ public:
             if (!wait)
             {
                 // drain the queue without running F on it
-                while (!m_queue.empty()) m_queue.pop();
+                while (!m_queue.empty())
+                {
+                    m_queue.pop();
+                }
             }
         }
 
         // notify all that we are done
         m_empty_cond.notify_all();
+        m_full_cond.notify_all();
 
-        for (auto i = 0; i < m_size; ++i)
+        for (auto i = 0; i < m_number_of_workers; ++i)
         {
-            // join on threads until they actually finish
-            // in case we are asked not to wait, we still join
-            // but since queue is empty the threads should exit immediatly
-            // TODO: check if we can detach in the wait=false case
             m_workers[i].join();
         }
     }
