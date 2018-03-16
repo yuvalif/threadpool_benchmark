b7207c4bfdb03c1168e1b80df62672c4fd697f71
Yuval Lifshitz <ylifshit@redhat.com>
2018-03-16 12:19:51 +0200

    dont fetch if not needed

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -2,9 +2,12 @@
 
 # get thread pool implementations to be benchmarked
 
-mkdir -p libs
-cd libs
-git clone https://github.com/vit-vit/CTPL.git
-git clone https://github.com/henkel/threadpool.git
-git clone https://github.com/progschj/ThreadPool.git
-cd -
+if [ ! -d "libs" ]; then
+    mkdir -p libs
+    cd libs
+    git clone https://github.com/vit-vit/CTPL.git
+    git clone https://github.com/henkel/threadpool.git
+    git clone https://github.com/progschj/ThreadPool.git
+    cd -
+fi
+
