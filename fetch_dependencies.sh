211096c01269bc06ab1feb4da6fb222baaf302ec
Yuval Lifshitz <ylifshit@redhat.com>
2018-03-18 09:57:57 +0200

    allow updating of git repos

--- fetch_dependencies.sh
+++ fetch_dependencies.sh
@@ -4,10 +4,18 @@
 
 if [ ! -d "libs" ]; then
     mkdir -p libs
-    cd libs
+    cd libs > /dev/null
     git clone https://github.com/vit-vit/CTPL.git
     git clone https://github.com/henkel/threadpool.git
     git clone https://github.com/progschj/ThreadPool.git
-    cd -
+    cd - > /dev/null
+else
+    for dir in ./libs/*
+    do
+        cd $dir > /dev/null
+        echo "Updating $dir..."
+        git pull
+        cd - > /dev/null
+    done
 fi
 
