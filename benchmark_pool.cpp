0ce22228e906a175c7dabd7a8b8d0130a9e25b4c
Yuval Lifshitz <ylifshit@redhat.com>
2018-03-18 10:57:19 +0200

    really fix the define issue

--- benchmark_pool.cpp
+++ benchmark_pool.cpp
@@ -9,8 +9,8 @@
 #include <chrono>   // for time calculations
 // thread libraries to be tested
 #include "CTPL/ctpl.h" // for testing CPTL thread pool
-#if BOOST_VERSION < 105000
-#define TIME_UTC_ TIME_UTC
+#ifndef TIME_UTC
+#define TIME_UTC TIME_UTC_
 #endif
 #include "threadpool/boost/threadpool.hpp" // for testing old boost thread pool (by Philippe Henkel)
 #include "ThreadPool/ThreadPool.h" // for testing thread pool using Jakob Progsch's thread pool
